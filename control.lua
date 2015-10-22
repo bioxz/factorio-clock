974cc3d39079a89d8deaf167f383f51d297310d4
Patrick Uven <patrick.uven@informatik.uni-oldenburg.de>
2015-10-22 02:27:38 +0200

    Initial commit - Added all needed informations to info.json - Simple ingame clock functionality in control.lua

---
+++ control.lua
@@ -0,0 +1,25 @@
+require "defines"
+
+script.on_event(defines.events.on_tick, function(event)
+	if not global.clock_enabled then
+		init()
+	end
+
+	update()
+end)
+
+function init()
+	game.players[1].gui.left.add{type = "label", name = "clock_game", caption = "Factorio Clock", style = "description_title_label_style"}
+	global.clock_enabled = 0
+end
+
+function update()
+	local time = math.fmod(game.daytime + 0.5, 1)
+	local hour = math.floor(time * 24)
+	local minute = math.floor((time * 24 - hour) * 60)
+	if minute < 10 then
+		minute = 0 .. minute
+	end
+	game.players[1].gui.left.clock_game.caption = hour .. ":" .. minute
+	
+end
